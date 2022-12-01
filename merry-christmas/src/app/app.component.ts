92979f8ba2a876f3b0ea69d8599ff8e3d4157119
Bruno Campos <campos.bruno1996@gmail.com>
2022-12-01 01:18:03 -0300

    first-commit

---
+++ merry-christmas/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy{
+  title = 'merry-christmas';
+
+  private subscription = new Subscription();
+  
+  public dateNow = new Date();
+  public dDay = new Date('Dec 31 2022 23:59:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference: any;
+  public secondsToDday: any;
+  public minutesToDday: any;
+  public hoursToDday: any;
+  public daysToDday: any;
+
+
+  private getTimeDifference () {
+      this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+      this.allocateTimeUnits(this.timeDifference);
+  }
+
+private allocateTimeUnits(timeDifference: any) {
+      this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+      this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+      this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+      this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+}
+
+  ngOnInit() {
+     this.subscription = interval(1000)
+         .subscribe(x => { this.getTimeDifference(); });
+  }
+
+ ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
+
+}
